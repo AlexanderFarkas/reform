51d67945d8d09df3f083587f57fa4154324e76a8
AlexanderFarkas <sashafarkas322@gmail.com>
2022-11-25 18:33:55 +0300

    fix!: add onChanged

--- lib/src/text_refield_builder.dart
+++ lib/src/text_refield_builder.dart
@@ -8,6 +8,7 @@ typedef TextRefieldWidgetBuilder = Widget Function(
 
 class TextRefieldBuilder extends StatefulWidget {
   final Refield<String> field;
+  final ValueChanged<String> onChanged;
   final ShowErrorPredicate? shouldShowError;
   final TextEditingController? controller;
   final TextRefieldWidgetBuilder builder;
@@ -16,8 +17,9 @@ class TextRefieldBuilder extends StatefulWidget {
     super.key,
     this.controller,
     required this.field,
-    required this.builder,
+    required this.onChanged,
     this.shouldShowError,
+    required this.builder,
   });
 
   @override
@@ -31,21 +33,30 @@ class _TextRefieldBuilderState extends State<TextRefieldBuilder> {
   void initState() {
     super.initState();
     controller.text = widget.field.value;
+    controller.addListener(_controllerListener);
   }
 
   @override
   void dispose() {
+    controller.removeListener(_controllerListener);
     if (widget.controller == null) {
       controller.dispose();
     }
     super.dispose();
   }
 
+  void _controllerListener() {
+    final controllerText = controller.text;
+    if (controllerText != widget.field.value) {
+      widget.onChanged(controllerText);
+    }
+  }
+
   @override
   void didUpdateWidget(covariant TextRefieldBuilder oldWidget) {
     final value = widget.field.value;
     if (value != controller.text) {
-      setState(() => controller.text = value);
+      controller.text = value;
     }
     super.didUpdateWidget(oldWidget);
   }
@@ -55,8 +66,7 @@ class _TextRefieldBuilderState extends State<TextRefieldBuilder> {
     return RefieldBuilder(
       field: widget.field,
       shouldShowError: widget.shouldShowError,
-      builder: (context, value, error) =>
-          widget.builder(context, controller, error),
+      builder: (context, value, error) => widget.builder(context, controller, error),
     );
   }
 }
