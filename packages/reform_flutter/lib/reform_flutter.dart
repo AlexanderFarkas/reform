48eca402405c7f114c4a26aac3bb7b403c1e86dd
AlexanderFarkas <sashafarkas322@gmail.com>
2022-11-28 16:31:16 +0300

    feat!: remove export

--- packages/reform_flutter/lib/reform_flutter.dart
+++ packages/reform_flutter/lib/reform_flutter.dart
@@ -3,8 +3,6 @@ library reform_flutter;
 import 'package:flutter/widgets.dart';
 import 'package:reform/reform.dart';
 
-export 'package:reform/reform.dart';
-
 part 'src/reform_scope.dart';
 part 'src/refield_builder.dart';
 part 'src/text_refield_builder.dart';
